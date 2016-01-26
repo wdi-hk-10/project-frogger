90456bf845965567676dfd63713f5679669055e5
Jon C Young II <youngii.jc@gmail.com>
2016-01-26 12:29:20 +0800

    set key events to move frogger - frogger moves once per key

--- js/application.js
+++ js/application.js
@@ -1,6 +1,46 @@
 $(document).ready(function() {
   console.log("Linked & Ready.")
 
+  var $body = $('body');
+  var $frogger = $('.frogger');
 
+  // start gameplay
+  $('.start-button').on('click', function(){
+    moveFrogger();
+  });
+
+  // move frogger
+  function moveFrogger() {
+    $body.keydown(function(e) {
+      // prevent page scrolling with arrow keys
+      // move frogger
+      if (e.keyCode == '40') {
+        // up arrow
+        e.preventDefault();
+        $frogger.animate({
+          top: "+50px",
+        });
+      } else if (e.keyCode == '38') {
+        // down arrow
+        e.preventDefault();
+        $frogger.animate({
+          bottom: "+50px",
+        });
+      } else if (e.keyCode == '37') {
+        // right arrow
+        e.preventDefault();
+        $frogger.animate({
+          right: "+50px",
+        });
+
+      } else if (e.keyCode == '39') {
+        // left arrow
+        e.preventDefault();
+        $frogger.animate({
+          left: "+50px",
+        });
+      };
+    });
+  };
 
 });
