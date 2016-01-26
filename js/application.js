900090a2e8bbd0e29087c2adbb570f11d545c79f
Jon C Young II <youngii.jc@gmail.com>
2016-01-26 17:07:02 +0800

    set gameboard boundaries for frogger

--- js/application.js
+++ js/application.js
@@ -11,32 +11,55 @@ $(document).ready(function() {
 
   // move frogger
   function moveFrogger() {
-    $body.keydown(function(e) {
+    $body.off().keydown(function(e) {
+      // check frogger is within game board
+      setBoundaries();
       // move frogger
       // prevent page scrolling with arrow keys
       if (e.keyCode == '38') {
-      // up arrow
-      $frogger.animate({ top: "-=50px" });
-      e.preventDefault();
+        // up arrow
+        if (parseInt($frogger.css('top')) > 5) {
+          $frogger.animate({ top: "-=50px" });
+        };
+        e.preventDefault();
 
       } else if (e.keyCode == '40') {
-      // down arrow
-      $frogger.animate({ top: "+=50px" });
-      e.preventDefault();
+        // down arrow
+        if (parseInt($frogger.css('top')) < 455) {
+          $frogger.animate({ top: "+=50px" });
+        };
+        e.preventDefault();
 
       } else if (e.keyCode == '39') {
-      // right arrow
-      $frogger.animate({ left: "+=50px" });
-      e.preventDefault();
+        // right arrow
+        if (parseInt($frogger.css('left')) < 750) {
+          $frogger.animate({ left: "+=50px" });
+        };
+          // WIP: hopping animation - CREATE CLASSES FOR FROGGER DIRECTION & HOP ANIMATION!
+          // } , {
+          //  step: function(now, fx) {
+          //     if (0.1 < fx.pos < 0.9) {
+          //       $(this).css('background-image', 'url("assets/frogger-jumping-sprite.png")');
+          //     } else if (fx.pos > 0.9) {
+          //       $(this).css('background-image', 'none');
+          //     }
+          //   }
+        e.preventDefault();
 
       } else if (e.keyCode == '37') {
-      // left arrow
-      $frogger.animate({ left: "-=50px" });
-      e.preventDefault();
+        // left arrow
+        if (parseInt($frogger.css('left')) > 0) {
+          $frogger.animate({ left: "-=50px" });
+        };
+        e.preventDefault();
       };
 
     console.log($frogger.css(["top", "right", "bottom", "left"]));
     });
   };
+
+  function setBoundaries(){
+
+  }
 
 });
