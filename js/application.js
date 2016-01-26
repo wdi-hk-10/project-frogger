32aaf1b915a8e1ab45c785ef93cc8a7436f2506d
Jon C Young II <youngii.jc@gmail.com>
2016-01-26 13:51:52 +0800

    refactored for readability

--- js/application.js
+++ js/application.js
@@ -12,41 +12,30 @@ $(document).ready(function() {
   // move frogger
   function moveFrogger() {
     $body.keydown(function(e) {
-      // prevent page scrolling with arrow keys
       // move frogger
-        if (e.keyCode == '38') {
-        // up arrow
-        console.log('up');
-        e.preventDefault();
-        $frogger.animate({
-          top: "-=50px",
-        });
+      // prevent page scrolling with arrow keys
+      if (e.keyCode == '38') {
+      // up arrow
+      $frogger.animate({ top: "-=50px" });
+      e.preventDefault();
 
-        } else if (e.keyCode == '40') {
-        // down arrow
-        console.log('down');
-        e.preventDefault();
-        $frogger.animate({
-          top: "+=50px",
-        });
+      } else if (e.keyCode == '40') {
+      // down arrow
+      $frogger.animate({ top: "+=50px" });
+      e.preventDefault();
 
-        } else if (e.keyCode == '39') {
-        // right arrow
-        console.log('right');
-        e.preventDefault();
-        $frogger.animate({
-          left: "+=50px",
-        });
+      } else if (e.keyCode == '39') {
+      // right arrow
+      $frogger.animate({ left: "+=50px" });
+      e.preventDefault();
 
-        } else if (e.keyCode == '37') {
-        // left arrow
-        console.log('left');
-        e.preventDefault();
-        $frogger.animate({
-          left: "-=50px",
-        });
+      } else if (e.keyCode == '37') {
+      // left arrow
+      $frogger.animate({ left: "-=50px" });
+      e.preventDefault();
       };
-      console.log($frogger.css(["top", "right", "bottom", "left"]));
+
+    console.log($frogger.css(["top", "right", "bottom", "left"]));
     });
   };
 
